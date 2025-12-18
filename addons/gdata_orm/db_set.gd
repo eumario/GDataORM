3537b038d4a86d2e12a0b96fab62270d0b52053f
Mario Steele <mario@ruby-im.net>
2025-12-18 16:58:49 -0600

    Updated DbSet
    Addeed Handle to Database we are working on with this DbSet.
    Switched from using klass to SQLiteObject to call these functions as we
    are already passing along class, and base class SQLiteObject can handle
    the meta programming.

--- addons/gdata_orm/db_set.gd
+++ addons/gdata_orm/db_set.gd
@@ -3,19 +3,28 @@ extends Resource
 class_name DbSet
 
 var klass: GDScript
-var db: SQLite
+var _db: SQLite
 
 func _init(h_klass: GDScript) -> void:
 	klass = h_klass
 
 func set_db(db_conn: SQLite) -> void:
-	db = db_conn
+	_db = db_conn
 
 func create_table(drop_if_exists: bool) -> void:
-	klass._create_table(db, klass, drop_if_exists)
+	SQLiteObject._create_table(_db, klass, drop_if_exists)
 
 func table_exists() -> bool:
-	return klass._table_exists(db, klass)
+	return SQLiteObject._table_exists(_db, klass)
 
 func has_id(id: Variant) -> bool:
-	return klass._has_id(db, klass, id)
+	return SQLiteObject._has_id(_db, klass, id)
+
+func find_one(conditions: Condition) -> SQLiteObject:
+	return SQLiteObject._find_one(_db, klass, conditions)
+
+func find_many(conditions: Condition) -> Array:
+	return SQLiteObject._find_many(_db, klass, conditions)
+
+func all() -> Array:
+	return SQLiteObject._all(_db, klass)
