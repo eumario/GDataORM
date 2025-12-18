3ebdba8666663c152e4b167ac7b479be0d203ab8
Mario Steele <mario@ruby-im.net>
2025-12-18 16:57:52 -0600

    Added Context
    This is the central point of the ORM system, allowing for DbSet to be
    setup, along with registering the classes.

---
+++ addons/gdata_orm/context.gd
@@ -0,0 +1,52 @@
+extends RefCounted
+class_name Context
+
+@export var file_path: String
+
+var db: SQLite
+
+func setup() -> void:
+	var props = get_property_list()
+	for prop in props:
+		if not prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
+			continue
+		if prop.type != TYPE_OBJECT or prop.class_name != "DbSet":
+			continue
+		var dbset: DbSet = get(prop.name)
+		dbset.klass.setup()
+
+func connect_to_db(db_path: String = "") -> void:
+	var props = get_property_list()
+	if db_path != "":
+		file_path = db_path
+	var db = SQLite.new()
+	db.path = file_path
+	db.open()
+	for prop in props:
+		if not prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
+			continue
+		if prop.type != TYPE_OBJECT or prop.class_name != "DbSet":
+			continue
+		var dbset: DbSet = get(prop.name)
+		dbset.set_db(db)
+
+func ensure_tables() -> void:
+	var props = get_property_list()
+	for prop in props:
+		if not prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
+			continue
+		if prop.type != TYPE_OBJECT or prop.class_name != "DbSet":
+			continue
+		var dbset: DbSet = get(prop.name)
+		if not dbset.table_exists():
+			dbset.create_table(false)
+
+func force_create_tables() -> void:
+	var props = get_property_list()
+	for prop in props:
+		if not prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
+			continue
+		if prop.type != TYPE_OBJECT or prop.class_name != "DbSet":
+			continue
+		var dbset: DbSet = get(prop.name)
+		dbset.create_table(true)
